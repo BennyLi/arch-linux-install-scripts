ad497e45dae5a5792f729762c9460b5f70a20a4b
Benny Li <dev@benny-li.de>
2020-02-19 09:41:03 +0000

    One script to handle them all :D

---
+++ base-os-setup.sh
@@ -0,0 +1,35 @@
+#! /usr/bin/env sh
+
+# Get this script via https:/git.io/fhlfO
+
+
+# Sources of this process:
+# * https://legacy.thomas-leister.de/arch-linux-luks-verschluesselt-auf-uefi-system-installieren-2/
+# * https://disconnected.systems/blog/archlinux-installer/#the-complete-installer-script
+
+
+source ./00_setup-variables.sh
+
+source ./01_intro-screen.sh
+
+source ./02_keyboard-setup.sh
+source ./05_internet-connect.sh
+
+source ./10_user-input.sh
+
+source ./20_drive-preperation.sh
+source ./21_drive-encryption.sh
+source ./22_drive-partitioning.sh
+source ./29_drive-mount.sh
+
+source ./50_package-repo-setup.sh
+source ./51_package-base.sh
+
+source ./60_base-system-config.sh
+source ./61_user-setup.sh
+
+source ./70_boot-configuration.sh
+
+source ./99_setup-finalize.sh
+
+exit 0
