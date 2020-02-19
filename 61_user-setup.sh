9225c8a70ccc0a371e3df165a091cc2e071dcc4a
Benny Li <dev@benny-li.de>
2020-02-19 09:40:26 +0000

    Added user setup incl shell and password

---
+++ 61_user-setup.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env sh
+
+arch-chroot /mnt useradd --create-home --user-group --shell /usr/bin/zsh --groups wheel,uucp,video,audio,storage,optical,games,input "$user"
+arch-chroot /mnt chsh -s /usr/bin/zsh
+
+echo "$user:$password" | chpasswd --root /mnt
+echo "root:$password" | chpasswd --root /mnt
+
+# TODO Move this to the ansible stuff
+cat <<EOF > /mnt/etc/sudoers.d/01_benny
+$user   ALL=(ALL) ALL
+$user   NOPASSWD: /usr/bin/halt,/usr/bin/poweroff,/usr/bin/reboot
+EOF
