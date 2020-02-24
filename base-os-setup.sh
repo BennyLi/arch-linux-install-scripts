116de38402002c6f68d0d04a25ecf570d69ce918
Benny Li <dev@benny-li.de>
2020-02-24 10:59:13 +0000

    Prepare first complete run

--- base-os-setup.sh
+++ base-os-setup.sh
@@ -10,31 +10,28 @@
 
 source ./00_setup-variables.sh
 
-#source ./01_intro-screen.sh
+source ./01_intro-screen.sh
 
-#source ./02_keyboard-setup.sh
-#source ./05_internet-connect.sh
+source ./02_keyboard-setup.sh
+source ./05_internet-connect.sh
 
 source ./10_user-input.sh
 
-#source ./20_drive-preperation.sh
-#source ./30_drive-encryption.sh
-#source ./40_drive-partitioning.sh
+source ./20_drive-preperation.sh
+source ./30_drive-encryption.sh
+source ./40_drive-partitioning.sh
 # TODO
 #source ./25_usb-pendrive-backup.sh
-#source ./49_drive-mount.sh
+source ./49_drive-mount.sh
 
-#source ./50_package-repo-setup.sh
-#source ./51_package-base.sh
+source ./50_package-repo-setup.sh
+source ./51_package-base.sh
 
 source ./60_base-system-config.sh
 source ./61_user-setup.sh
-exit 0
 
 source ./70_boot-configuration.sh
 
 source ./99_setup-finalize.sh
-
-exit 0
 
 # vim: set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab
