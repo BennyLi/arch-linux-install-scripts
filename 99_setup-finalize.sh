72cd19cba2c4916fa90559f2528cdc30f5948b87
Benny Li <dev@benny-li.de>
2020-02-25 11:34:38 +0100

    Unmount and close all lvm and luks things

--- 99_setup-finalize.sh
+++ 99_setup-finalize.sh
@@ -4,6 +4,10 @@ umount /mnt/boot
 umount /mnt/efi
 umount /mnt
 
+vgchange -a n
+cryptsetup luksClose $LUKS_BOOT_DEVICE_NAME
+cryptsetup luksClose $LUKS_DEVICE_NAME
+
 echo "All done!"
 read -p "Remove the USB stick and press [Enter] to reboot to your new system..."
 reboot
